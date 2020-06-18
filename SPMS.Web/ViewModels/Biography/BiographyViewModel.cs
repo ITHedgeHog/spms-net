d1cace61007f5160c9d476f59bdbe88bffb9dde9
Dan Taylor <devops@stevethehedgehog.com>
2020-06-18 23:35:18 +0100

    Fix Biography 500 Error

---
+++ SPMS.Web/ViewModels/Biography/BiographyViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPMS.Web.ViewModels.Biography
+{
+    public class BiographyViewModel : ViewModel
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+
+        public string DateOfBirth { get; set; }
+        public string Species { get; set; }
+        public string Homeworld { get; set; }
+        public string Gender { get; set; }
+        public string Born { get; set; }
+        public string Eyes { get; set; }
+        public string Hair { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public string Affiliation { get; set; }
+        public string Assignment { get; set; }
+        public string Rank { get; set; }
+        public string RankImage { get; set; }
+
+        public int StatusId { get; set; }
+        public string Status { get; set; }
+
+        public int PostingId { get; set; }
+        public string Posting { get; set; }
+
+        public string Owner { get; set; }
+        public int PlayerId { get; set; }
+        public string Player { get; set; }
+        public string History { get; set; }
+    }
+}
