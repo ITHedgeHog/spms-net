45f4d45f90dee11fb1fbd973a0565782ddb5fff5
Dan Taylor <devops@stevethehedgehog.com>
2020-06-19 23:39:50 +0100

    Add Admin menu

--- SPMS.Web/Service/UserService.cs
+++ SPMS.Web/Service/UserService.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using SPMS.Web.Models;
 
 namespace SPMS.Web.Service
 {
     public class UserService : IUserService
     {
         private readonly IHttpContextAccessor _httpContext;
+        private readonly SpmsContext _context;
 
-        public UserService(IHttpContextAccessor httpContext)
+        public UserService(IHttpContextAccessor httpContext, SpmsContext context)
         {
             _httpContext = httpContext;
+            _context = context;
         }
 
         public string GetAuthId()
@@ -28,12 +32,14 @@ namespace SPMS.Web.Service
 
         public bool IsPlayer()
         {
-            return _httpContext.HttpContext.User.IsInRole(StaticValues.PlayerRole);
+            var user = _context.Player.Include(p => p.Roles).ThenInclude(role => role.PlayerRole).FirstOrDefault(x => x.AuthString == GetAuthId());
+            return user != default(Player) && user.Roles.Any(u => u.PlayerRole.Name == StaticValues.PlayerRole);
         }
 
         public bool IsAdmin()
         {
-            return _httpContext.HttpContext.User.IsInRole(StaticValues.AdminRole);
+            var user = _context.Player.Include(p => p.Roles).ThenInclude(role => role.PlayerRole).FirstOrDefault(x => x.AuthString == GetAuthId());
+            return user != default(Player) && user.Roles.Any(u => u.PlayerRole.Name == StaticValues.AdminRole);
         }
     };
     public interface IUserService
