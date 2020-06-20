90aa1c8c244fcbf9c0de05a41f3dbb89d9a6b20a
Dan Taylor <devops@stevethehedgehog.com>
2020-06-20 01:01:26 +0100

    Show debug env vars

--- SPMS.Web/Controllers/DebugController.cs
+++ SPMS.Web/Controllers/DebugController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SPMS.Web.ViewModels;
 
 namespace SPMS.Web.Controllers
 {
@@ -10,7 +11,8 @@ namespace SPMS.Web.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var vm = new ViewModel();
+            return View(vm);
         }
     }
 }
