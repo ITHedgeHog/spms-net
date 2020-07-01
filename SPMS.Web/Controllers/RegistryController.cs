2b59440ad55948e59872078293ce1d16d38318ca
Dan Taylor <devops@stevethehedgehog.com>
2020-07-01 18:43:54 +0100

    Put Ship Registry behind a Feature Flag until its complete.

--- SPMS.Web/Controllers/RegistryController.cs
+++ SPMS.Web/Controllers/RegistryController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement.Mvc;
 using SPMS.Application.ViewModels;
+using SPMS.Common;
 
 namespace SPMS.Web.Controllers
 {
+    [FeatureGate(FeatureFlags.Registry)]
     public class RegistryController : Controller
     {
         public IActionResult Index()
