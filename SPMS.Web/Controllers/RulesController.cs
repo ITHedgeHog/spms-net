ad95994d8f5cc9bf7c155ae36e8078abdbb73948
Evan Scown <sir.lagalot.thegreat@gmail.com>
2020-07-15 21:08:58 +1000

    Fix for issues 81 & 82

--- SPMS.Web/Controllers/RulesController.cs
+++ SPMS.Web/Controllers/RulesController.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SPMS.Application.Services;
+using SPMS.ViewModel.Rules;
+using SPMS.Web.Models;
+using SPMS.Web.Service;
 
 namespace SPMS.Web.Controllers
 {
     public class RulesController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var vm = new RulesViewModel();
+            return View(vm);
         }
     }
 }
