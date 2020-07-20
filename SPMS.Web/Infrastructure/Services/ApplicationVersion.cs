73e87f3a50b9d88f4fc9142ece19fbb0d872e9c7
Dan Taylor <devops@stevethehedgehog.com>
2020-07-20 02:06:48 +0100

    Use assembly version

--- SPMS.Web/Infrastructure/Services/ApplicationVersion.cs
+++ SPMS.Web/Infrastructure/Services/ApplicationVersion.cs
@@ -5,8 +5,7 @@ namespace SPMS.Web.Infrastructure.Services
 {
     public class ApplicationVersion : IApplicationVersion
     {
-        public string Version => Assembly.GetEntryAssembly()!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-            ?.InformationalVersion ?? "0.0.0";
+        public string Version => Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
         public string InformationVersion => Assembly.GetEntryAssembly()!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
             ?.InformationalVersion ?? "0.0.0";
     }
