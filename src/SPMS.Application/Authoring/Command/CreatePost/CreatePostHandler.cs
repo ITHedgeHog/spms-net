d19727e474a1e826efcdbcbf94d565e502b983e3
Dan Taylor <devops@stevethehedgehog.com>
2020-08-01 17:28:09 +0100

    Fix create post command because I'm a numpty.

--- src/SPMS.Application/Authoring/Command/CreatePost/CreatePostHandler.cs
+++ src/SPMS.Application/Authoring/Command/CreatePost/CreatePostHandler.cs
@@ -1,9 +1,12 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using SPMS.Application.Common.Interfaces;
+using SPMS.Application.Dtos;
 using SPMS.Common;
 using SPMS.Domain.Models;
 
@@ -16,18 +19,24 @@ namespace SPMS.Application.Authoring.Command.CreatePost
         public class CreatePostHandler : IRequestHandler<CreatePost, int>
         {
             private readonly ISpmsContext _db;
+            private readonly ITenantAccessor<TenantDto> _tenant;
+            private readonly IUserService _userService;
 
-            public CreatePostHandler(ISpmsContext db)
+            public CreatePostHandler(ISpmsContext db, ITenantAccessor<TenantDto> tenant, IUserService userService)
             {
                 _db = db;
+                _tenant = tenant;
+                _userService = userService;
             }
 
             public async Task<int> Handle(CreatePost request, CancellationToken cancellationToken)
             {
-                var activeEpisodeId = await _db.Episode.Include(e => e.Status).CountAsync(e => e.Status.Name == StaticValues.Published || e.Status.Name == StaticValues.Archived, cancellationToken: cancellationToken);
-
-
-
+                var gameId = _tenant.Instance.Id;
+                var activeEpisodeId = await _db.Episode
+                    .Include(e => e.Series)
+                    .Include(e => e.Status)
+                    .CountAsync(e => e.Series.GameId == gameId && (e.Status.Name == StaticValues.Published || e.Status.Name == StaticValues.Archived), cancellationToken: cancellationToken);
+                
                 var entity = new EpisodeEntry();
                 var episodeEntryStatus = 
                 entity.EpisodeEntryStatusId = (await _db.EpisodeEntryStatus.FirstAsync(
@@ -36,9 +45,14 @@ namespace SPMS.Application.Authoring.Command.CreatePost
                 entity.EpisodeEntryTypeId = (await _db.EpisodeEntryType.FirstAsync(x => x.Name == StaticValues.Post,
                     cancellationToken: cancellationToken)).Id;
                 entity.EpisodeId = activeEpisodeId;
-
                 await _db.EpisodeEntry.AddAsync(entity, cancellationToken: cancellationToken);
                 await _db.SaveChangesAsync(cancellationToken);
+                entity.EpisodeEntryPlayer =
+                    new Collection<EpisodeEntryPlayer>()
+                        {new EpisodeEntryPlayer() {PlayerId = _userService.GetId(), EpisodeEntryId = entity.Id}};
+                _db.EpisodeEntry.Update(entity);
+                await _db.SaveChangesAsync(cancellationToken);
+
 
                 return entity.Id;
             }
--- test/SPMS.Test/Authoring/Command/AuthorPostCommandTests.cs
+++ test/SPMS.Test/Authoring/Command/AuthorPostCommandTests.cs
@@ -1,28 +1,66 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading;
 using MediatR;
 using Moq;
 using Shouldly;
 using SPMS.Application.Authoring.Command.CreatePost;
+using SPMS.Application.Common.Interfaces;
+using SPMS.Application.Dtos;
 using SPMS.Application.Tests.Common;
+using SPMS.Persistence.MSSQL;
 using Xunit;
 
 namespace SPMS.Application.Tests.Authoring.Command
 {
-    public class AuthorPostCommandTests : CommandTestBase
+    public class AuthorPostCommandTests : IClassFixture<AuthorPostCommandTestsFixture>
     {
+        private readonly ISpmsContext _db;
+        private readonly ITenantAccessor<TenantDto> _tenant;
+        private readonly IUserService _userService;
+        public AuthorPostCommandTests(AuthorPostCommandTestsFixture fixture)
+        {
+            _db = fixture.Context;
+            _tenant = fixture.TenantAccessor;
+            _userService = fixture.UserService;
+        }
+
         [Fact]
         public async void Should_Create_New_Post()
         {
             var mediatorMock = new Mock<IMediator>();
-            var sut = new CreatePost.CreatePostHandler(Context);
+            var sut = new CreatePost.CreatePostHandler(_db, _tenant,  _userService);
 
 
             var result = await sut.Handle(new CreatePost(), CancellationToken.None);
 
             result.ShouldBe<int>(1);
 
-            Context.EpisodeEntry.Count().ShouldBe(1);
+            _db.EpisodeEntry.Count().ShouldBe(1);
+        }
+    }
+    public class AuthorPostCommandTestsFixture : IDisposable
+    {
+        public SpmsContext Context { get; set; }
+        public ITenantAccessor<TenantDto> TenantAccessor { get; set; }
+        public IUserService UserService { get; set; }
+
+        public AuthorPostCommandTestsFixture()
+        {
+            Context = TestSpmsContextFactory.Create();
+
+            var mockTenantAccessor = new Mock<ITenantAccessor<TenantDto>>();
+            mockTenantAccessor.Setup(x => x.Instance).Returns(new TenantDto() {Id = 1, GameName = "Test Game"});
+            TenantAccessor = mockTenantAccessor.Object;
+
+            var mockUserService = new Mock<IUserService>();
+            mockUserService.Setup(x => x.GetId()).Returns(1);
+            UserService = mockUserService.Object;
+        }
+
+        public void Dispose()
+        {
+            Context?.Dispose();
         }
     }
 }
